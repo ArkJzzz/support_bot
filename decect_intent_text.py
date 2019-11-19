92346c5ff638373434ba0e9c36e2ca2d6503bf2f
ArkJzzz <arkjzzz@gmail.com>
2019-11-19 19:22:01 +0300

    подружил: бот в Telegram отвечает фразами из DialogFlow

---
+++ decect_intent_text.py
@@ -0,0 +1,86 @@
+#!usr/bin/python3
+
+# Copyright 2017 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""DialogFlow API Detect Intent Python sample with text inputs.
+
+Examples:
+  python detect_intent_texts.py -h
+  python detect_intent_texts.py --project-id PROJECT_ID \
+  --session-id SESSION_ID \
+  "hello" "book a meeting room" "Mountain View"
+  python detect_intent_texts.py --project-id PROJECT_ID \
+  --session-id SESSION_ID \
+  "tomorrow" "10 AM" "2 hours" "10 people" "A" "yes"
+"""
+
+import argparse
+import uuid
+import logging
+
+# [START dialogflow_detect_intent_text]
+def detect_intent_text(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+    import dialogflow_v2 as dialogflow
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.types.TextInput(
+        text=text, language_code=language_code)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        session=session, query_input=query_input)
+
+    logging.debug('Query text: {}'.format(response.query_result.query_text))
+    logging.debug('Detected intent: {} (confidence: {})\n'.format(
+        response.query_result.intent.display_name,
+        response.query_result.intent_detection_confidence))
+    logging.debug('Fulfillment text: {}\n'.format(
+         response.query_result.fulfillment_text))
+
+    return response.query_result.fulfillment_text
+# [END dialogflow_detect_intent_text]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument(
+        '--project-id',
+        help='Project/agent id.  Required.',
+        required=True)
+    parser.add_argument(
+        '--session-id',
+        help='Identifier of the DetectIntent session. '
+        'Defaults to a random UUID.',
+        default=str(uuid.uuid4()))
+    parser.add_argument(
+        '--language-code',
+        help='Language code of the query. Defaults to "en-US".',
+        default='en-US')
+    parser.add_argument(
+        'texts',
+        nargs='+',
+        type=str,
+        help='Text inputs.')
+
+    args = parser.parse_args()
+
+    detect_intent_texts(
+        args.project_id, args.session_id, args.texts, args.language_code)
---
+++ support_bot.py
@@ -0,0 +1,63 @@
+#!usr/bin/python3
+
+import requests
+import os
+import sys
+import argparse
+import logging
+import telegram
+import detect_intent_text
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import Filters
+from telegram.ext import MessageHandler
+from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
+
+
+
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="start_handler: Здравствуйте.")
+
+
+def textMessage(bot, update):
+    dialogflow_token = os.getenv("DIALOGFLOW_TOKEN")
+    project_id = 'arkjzzz-dvmn-support-bot-gehlt'
+    chat_id = update.message.chat_id
+    language_code = 'ru-RU'
+    text = update.message.text
+
+    fulfillment_text = detect_intent_text(project_id, chat_id, text, language_code)
+
+    logging.debug('Message text: {}\n'.format(text))
+    logging.debug('Fulfillment text: {}\n'.format(fulfillment_text))
+    
+    bot.sendMessage(chat_id, fulfillment_text)
+    
+
+def main():
+    # init
+    logging.basicConfig(format = u'[LINE:%(lineno)d]#  %(message)s', level = logging.DEBUG)
+
+    load_dotenv()
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'df_key.json'
+    telegram_token = os.getenv("TELEGRAM_TOKEN")
+    bot = telegram.Bot(token=telegram_token)
+    updater = Updater(token=telegram_token)
+
+
+    # do
+    start_handler = CommandHandler('start', start)
+    updater.dispatcher.add_handler(start_handler)
+
+    text_massage_handler = MessageHandler(Filters.text, textMessage)
+    updater.dispatcher.add_handler(text_massage_handler)
+
+    updater.start_polling()
+
+    # Останавливаем бота, если были нажаты Ctrl + C
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
